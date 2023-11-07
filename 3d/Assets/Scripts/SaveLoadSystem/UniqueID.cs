bdbb399be503b1e3521f30701a9dc58737f8d5e2
ColeR1 <crichards24@student.dimanregional.org>
2023-11-07 14:24:26 -0500

    unique id working

--- 3d/Assets/Scripts/SaveLoadSystem/UniqueID.cs
+++ 3d/Assets/Scripts/SaveLoadSystem/UniqueID.cs
@@ -18,8 +18,9 @@ public class UniqueID : MonoBehaviour
 
     public string ID => _id;
 
-    private void OnValidate()
-    {
+    private void Awake() {
+        if(idDatabase == null) idDatabase = new SerializableDictionary<string, GameObject>();
+
         if(idDatabase.ContainsKey(_id)) Generate();
         else idDatabase.Add(_id, this.gameObject);
     }
